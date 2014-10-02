ada95f3c10f596e85d177d528730cba235a0da2f
jmjvvv <jmjvvv@gmail.com>
2014-10-02 16:39:54 +0900

    d

--- sketch.js
+++ sketch.js
@@ -15,19 +15,19 @@ function draw()
 
 
   if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
+  ellipse(50, 300, 100, 100);
 } else if (mouseX < 200){
-  ellipse(150, 200, 150, 150);
+  ellipse(150, 300, 150, 150);
 } else if (mouseX < 300){
-  ellipse(250, 200, 200, 200);
+  ellipse(250, 300, 200, 200);
 } else if (mouseX < 400){
-  ellipse(350, 200, 250, 250);
+  ellipse(350, 300, 250, 250);
 } else if (mouseX < 500){
-  ellipse(450, 200, 300, 300);
+  ellipse(450, 300, 300, 300);
 } else if( mouseX < 600){
-  ellipse(550, 200, 350, 350);
+  ellipse(550, 300, 350, 350);
 } else {
-  ellipse(650, 200, 400, 400);
+  ellipse(650, 300, 400, 400);
 }
 
 }
